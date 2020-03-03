f82938c94f69c80db5013dbd794f87d89ecfaf5d
123456 <123456@example.com>
2020-03-03 20:09:41 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -5,33 +5,34 @@
       <img src="../../assets/img/logo_admin.png" alt="">
     </div>
     <!-- 导航菜单 -->
-    <el-menu background-color="#353b4e" text-color="#adafb5">
-      <el-menu-item>
+    <!-- router 开启路由模式 以每项的index 作为path地址 -->
+    <el-menu router background-color="#353b4e" text-color="#adafb5">
+      <el-menu-item index="/home">
         <i class="el-icon-s-home"></i>
         <span>首页</span>
       </el-menu-item>
       <!-- 有下拉选项用 el-submenu -->
-      <el-submenu>
+      <el-submenu index='1'>
         <template slot="title">
           <i class="el-icon-edit"></i>
           <span>内容管理</span>
         </template>
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>内容列表</el-menu-item>
-        <el-menu-item>评论列表</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">文章列表</el-menu-item>
+        <el-menu-item index="/home/comment">评论列表</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
       </el-submenu>
-      <el-submenu>
+      <el-submenu index='2'>
         <template slot="title">
           <i class="el-icon-setting"></i>
           <span>粉丝管理</span>
         </template>
-        <el-menu-item>图文数据</el-menu-item>
-        <el-menu-item>粉丝概况</el-menu-item>
-        <el-menu-item>粉丝画像</el-menu-item>
-        <el-menu-item>粉丝列表</el-menu-item>
+        <el-menu-item index="/home/picture">图文数据</el-menu-item>
+        <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+        <el-menu-item index="/home/fansphoto">粉丝画像</el-menu-item>
+        <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
       </el-submenu>
-      <el-menu-item>
+      <el-menu-item index='/home/account'>
         <i class="el-icon-user"></i>
         <span>账户信息</span>
       </el-menu-item>
