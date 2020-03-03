71b0e205e974b4db78458168ac6c21dad4b09595
123456 <123456@example.com>
2020-03-03 18:48:14 +0800

    主页模块-头部结构及下拉菜单

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -1,11 +1,29 @@
 <template>
   <!-- layout布局 -->
-  <el-row>
-    <el-col :span="12">
+  <el-row class="layout-header" type="flex" align="middle">
+    <!-- 左侧 -->
+    <el-col :span="12" class="left">
       <i class="el-icon-s-fold"></i>
       <span>江苏传智播客教育科技股份有限公司</span>
     </el-col>
-    <el-col :span="12"></el-col>
+
+    <!-- 右侧 -->
+    <el-col :span="12" class="right">
+      <!-- 在放置一个el-row使右侧靠右对齐 -->
+      <el-row type="flex" aligin="middle" justify="end">
+        <img src="../../assets/img/avatar.jpg" alt="">
+
+        <!-- 下拉选项 -->
+        <el-dropdown trigger="click">
+          <span>用户昵称</span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>git地址</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-row>
+    </el-col>
   </el-row>
 </template>
 
@@ -15,6 +33,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.layout-header {
+  height: 60px;
+  .left {
+    i {
+      // 调整图标基线对齐
+      vertical-align: middle;
+      font-size: 23px;
+    }
+  }
+  .right {
+    img {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      margin-right:5px;
+    }
+  }
+
+}
 
 </style>
