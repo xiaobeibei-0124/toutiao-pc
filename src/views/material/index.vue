e571b0447c6c11852b431db4c7600171cb728df5
123456 <123456@example.com>
2020-03-06 22:35:16 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -29,6 +29,14 @@
         </div>
       </el-tab-pane>
     </el-tabs>
+    <!-- 一个公共的分页栏 -->
+    <el-row type="flex" align="middle" justify="center" style="height:80px;">
+      <el-pagination @current-change='changePage' background layout="prev, pager, next"
+       :total="page.total"
+       :page-size='page.pageSize'
+       :current-page="page.currentPage">
+      </el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -37,12 +45,23 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        total: 0, // 总页数
+        pageSize: 8, // 每页显示几条
+        currentPage: 1 // 默认显示第一页
+      }
     }
   },
   methods: {
+    // 点击分页获取当前点击页数
+    changePage (newpage) {
+      this.page.currentPage = newpage
+      this.getMaterial()
+    },
     // 监听标签变换 当标签变换是重新拉取数据
     changeTab () {
+      this.page.currentPage = 1 // 当切换选项时，分页应位于首页
       this.getMaterial()
     },
     getMaterial () {
@@ -50,11 +69,16 @@ export default {
         url: '/user/images',
         method: 'get',
         params: { // query和header中的参数
-          collect: this.activeName === 'collect' // 传递的是布尔值 所以看现在点击的活动名称是否是收藏的
+          collect: this.activeName === 'collect', // 传递的是布尔值 所以看现在点击的活动名称是否是收藏的
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
+
         },
         data: {} // body中的参数
       }).then(res => {
         this.list = res.data.results
+        // 获取完数据 将总数传给page
+        this.page.total = res.data.total_count
       }).catch(() => {
         this.$message.error('请求错误')
       })
